c57fe5df5050477401b7add35dbdfc3b055e2dd3
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-03 18:20:00 -0300

    Criar o serviço de carrinho de compras
    - Gerenciando dados:
              - Criar o serviço de carrinho de compras
              - Definir um serviço de carrinho
              - Use o serviço de carrinho

--- Projeto_basico/src/app/product-details/product-details.component.ts
+++ Projeto_basico/src/app/product-details/product-details.component.ts
@@ -2,16 +2,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
+
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('You product has been added to the cart!');
+  }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;

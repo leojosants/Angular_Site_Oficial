377b9fd3ff5ba70bd83d7ce256678a8883d7fa9e
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-03 19:10:32 -0300

    Recuperar preços de envio
    - Gerenciando dados:
         - Recuperar preços de envio
              - Configurar AppModulepara usarHttpClient
              - Configurar CartServicepara usarHttpClient
              - Configure CartServicepara obter preços de envio

--- Projeto_basico/src/app/app.module.ts
+++ Projeto_basico/src/app/app.module.ts
@@ -1,4 +1,6 @@
 // 
+
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -13,6 +15,7 @@ import { CartComponent } from './cart/cart.component';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
--- Projeto_basico/src/app/cart.service.ts
+++ Projeto_basico/src/app/cart.service.ts
@@ -1,6 +1,7 @@
 // 
 
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -9,7 +10,10 @@ import { Product } from './products';
 
 export class CartService {
   items: Product[] = [];
-  constructor() { }
+  
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -22,6 +26,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>
+    ('/assets/shipping.json');
   }
 
 }

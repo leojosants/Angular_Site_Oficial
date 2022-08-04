45e32cd8d64d076c36f9973f7c945d699eeb0580
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-04 04:51:52 -0300

    Exibir uma lista de seleção
    Passos realizados:
         - Crie heróis simulados
    
         - Exibindo heróis
              - Listar heróis com*ngFor
              - Estilize os heróis
    
         - Visualizando detalhes
              - Adicionar uma associação de evento de clique
              - Adicione o manipulador de eventos de clique
              - Adicionar uma seção de detalhes
              - Estilize o herói selecionado

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +11,17 @@ import { Hero } from '../hero';
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
 
   constructor() { }
 
